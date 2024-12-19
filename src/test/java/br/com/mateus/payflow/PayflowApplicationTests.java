2458341c4f0dffdd885458eac7f9f1bbd16cfa98
Mateus Dev <porcruxe@gmail.com>
2024-12-19 19:32:30 -0300

    first commit

---
+++ src/test/java/br/com/mateus/payflow/PayflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateus.payflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
